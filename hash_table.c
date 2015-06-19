8a064f33a5224f3d72c9455cd87d537a9cc953d7
Paul Sonnenschein <sonnenschein@soprop.ch>
2015-06-19 13:56:14 +0200

    added hash_table base

---
+++ hash_table.c
@@ -0,0 +1,5 @@
+//
+// Created by md55 on 19.06.2015.
+//
+
+#include "hash_table.h"
---
+++ hash_table.h
@@ -0,0 +1,8 @@
+//
+// Created by md55 on 19.06.2015.
+//
+
+#ifndef DATASTRUCTURES_HASH_TABLE_H
+#define DATASTRUCTURES_HASH_TABLE_H
+
+#endif //DATASTRUCTURES_HASH_TABLE_H
